d9b196c5ae8f5cd047843955cfd156e151e07688
JudahEllis4 <judahellis44@gmail.com>
2026-01-12 13:10:26 -0800

    making ui work

--- Monster Mash/Monster Mash/Assets/Test Rooms/Judah/MultiTest/PLayerMoveTemp.cs
+++ Monster Mash/Monster Mash/Assets/Test Rooms/Judah/MultiTest/PLayerMoveTemp.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PLayerMoveTemp : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     private Vector2 moveInput;
+
+    [SerializeField]
+    private bool clickerMode = false; //mouse or hand, player is in menu navigation mode, not actively a monster or in combat
+
+    private Scene UIscene;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +25,61 @@ public class PLayerMoveTemp : MonoBehaviour
         moveInput = input.Get<Vector2>();
     }
 
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        if (clickerMode)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         Vector3 move = new Vector3(moveInput.x, moveInput.y);
         transform.Translate(move * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    void OnEnable()
+    {
+        SceneManager.activeSceneChanged += OnActiveSceneChanged;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.activeSceneChanged -= OnActiveSceneChanged;
+    }
+
+    void OnActiveSceneChanged(Scene oldScene, Scene newScene)
+    {
+        Debug.Log("Active scene changed from " + oldScene.name + " to " + newScene.name);
+        // Your code here to run when the scene changes
+        SwitchPlayerMode();
+    }
+
+    private void SwitchPlayerMode()
+    {
+        if (UIscene == SceneManager.GetActiveScene())
+        {
+            PlayerUIMode();
+        }
+        else
+        {
+            PlayerMonsterMode();
+        }
+    }
+
+    private void PlayerUIMode()
+    {
+        clickerMode = true;
+    }
+
+    private void PlayerMonsterMode()
+    {
+        clickerMode = false;
     }
 }
